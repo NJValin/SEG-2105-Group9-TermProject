21fceb254a6128aef99b24a21376469cb9a91135
Neil Valin <nvali069@uottawa.ca>
2022-10-27 23:34:43 -0400

    Working on MainActivity. Can collect username/password

--- README.txt
+++ README.txt
@@ -1,4 +1,8 @@
 References:
 	-Layout of login page
-	-Youtube: CodingWithMitch
-	-https://www.youtube.com/watch?v=mtYBBInIIE8
+		-Youtube: CodingWithMitch
+		-https://www.youtube.com/watch?v=mtYBBInIIE8
+	-Using SQLite for login
+		-Youtube: BTech Days
+		-https://www.youtube.com/watch?v=o9CVZ1gQgQo&t=1200s
+		
--- app/src/main/java/com/example/seg2105_termprojectcourseenrollmentapp/MainActivity.java
+++ app/src/main/java/com/example/seg2105_termprojectcourseenrollmentapp/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.seg2105_termprojectcourseenrollmentapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputLayout;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText userName, password;
+    Button signIn;
+    TextView successfulSignIn;
+    DBHelper db;
+    boolean validAccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userName = (EditText) findViewById(R.id.userName);
+        password = (EditText) findViewById(R.id.passWord);
+        signIn = (Button) findViewById(R.id.signInButton);
+        successfulSignIn = (TextView) findViewById(R.id.loginSuccessful);
+
+        db = new DBHelper(this);
+        signIn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        String usrname = userName.getText().toString();
+        String passWord = password.getText().toString();
+        if (usrname.equals("")||passWord.equals("")) {
+            successfulSignIn.setText("Please enter a valid username or password");
+            userName.setText("");
+            password.setText("");
+        }
+        if (db.checkLogin(usrname, passWord)) {
+            validAccount=true;
+        }
     }
 }
